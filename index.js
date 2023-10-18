b7ab5048d962fc2cd81d1fa4f162fc619f6944c0
afifa <2023afifa@gmail.com>
2023-10-19 02:20:41 +0600

    Created username and password

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.BS_USER}:${process.env.BS_PASS}@cluster0.etbjr0z.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get("/", (req, res) => {
     res.send("Brand shop is running");
